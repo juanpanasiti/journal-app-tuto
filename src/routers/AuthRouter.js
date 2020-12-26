1dd5b9840bb73ab9f127e05b0673735b643a47ae
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-12-26 20:42:09 -0300

    Estilos de login

--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -5,13 +5,15 @@ import { RegisterScreen } from '../components/auth/RegisterScreen'
 
 export const AuthRouter = () => {
     return (
-        <div>
-            <Switch>
-                <Route path='/auth/register' component={RegisterScreen} />
-                <Route path='/auth/login' component={LoginScreen} />
-
-                <Redirect to='/auth/login' />
-            </Switch>
+        <div className='auth__main'>
+            <div className='auth__box-container'>
+                <Switch>
+                    <Route path='/auth/register' component={RegisterScreen} />
+                    <Route path='/auth/login' component={LoginScreen} />
+    
+                    <Redirect to='/auth/login' />
+                </Switch>
+            </div>
         </div>
     )
 }
