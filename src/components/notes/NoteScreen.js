2af85687e22dea371a87ec8372ef07b056acd7d1
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-12-28 00:38:06 -0300

    Estructura y diseño de NoteScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,16 @@ export const NoteScreen = () => {
     return (
         <div className='notes__main-content'>
             <NotesAppBar />
+
+            <div className='notes__content'>
+                <input type='text' placeholder='Some awesome title' className='notes__title-input'/>
+
+                <textarea placeholder='What happend today?' className='notes_textarea' />
+            </div>
+
+            <div className='notes__image' >
+                <img src='https://www.etonline.com/sites/default/files/styles/max_1280x720/public/images/2020-05/star-wars-the-skywalker-saga-1280.png?h=c673cd1c&itok=m_ee5xuz' alt='imagen' />
+            </div>
         </div>
     )
 }
