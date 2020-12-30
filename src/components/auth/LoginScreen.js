7199ad8dffaa09873eb027aad78a12545eeb0eb1
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-12-30 17:54:50 -0300

    Login con google (firebase)

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,21 +1,42 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { startGoogleLogin, startLoginEmailPassword } from '../../actions/auth';
+import { useForm } from '../../hooks/useForm';
 
 export const LoginScreen = () => {
+    const dispatch = useDispatch();
+
+    const [formValues, handleInputChange] = useForm({
+        email: '',
+        password: '',
+    });
+
+    const { email, password } = formValues;
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        dispatch(startLoginEmailPassword(email, password));
+    };
+
+    const handleGoogleLogin = () => {
+        dispatch(startGoogleLogin());
+    };
     return (
         <>
             <h3 className='auth__title'>Login</h3>
 
-            <form>
-                <input type='text' placeholder='E-mail' name='email' className='auth__input' autoComplete='off' />
-                <input type='password' placeholder='Password' name='password' className='auth__input' />
+            <form onSubmit={handleLogin}>
+                <input type='text' placeholder='E-mail' name='email' className='auth__input' autoComplete='off' value={email} onChange={handleInputChange} />
+                <input type='password' placeholder='Password' name='password' className='auth__input' value={password} onChange={handleInputChange} />
 
-                <button type='submit' className='btn btn-primary btn-block' >Login</button>
-
+                <button type='submit' className='btn btn-primary btn-block'>
+                    Login
+                </button>
 
                 <div className='auth__social-networks'>
                     <p>Login with social networks</p>
-                    <div className='google-btn'>
+                    <div className='google-btn' onClick={handleGoogleLogin}>
                         <div className='google-icon-wrapper'>
                             <img
                                 className='google-icon'
