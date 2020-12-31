7c41a5eaab940b29de8969d9c7c0f7feb4d7d64e
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-12-31 19:12:39 -0300

    Deshabilitado de boton Login mientras se logea

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,8 +1,12 @@
 import { firebase, googleAuthProvider } from '../firebase/firebase-config';
 import { types } from '../types/types';
+import { finishLoading, startLoading } from './ui';
 
 export const startLoginEmailPassword = (email, password) => {
     return (dispatch) => {
+        
+        dispatch(startLoading())
+
         firebase
             .auth()
             .signInWithEmailAndPassword(email, password)
@@ -11,7 +15,10 @@ export const startLoginEmailPassword = (email, password) => {
             })
             .catch((e) => {
                 console.log(e);
-            });
+            })
+            .finally( () => {
+                dispatch( finishLoading() )
+            })
 
         // dispatch(login(123, 'Pedro'));
     };
--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { startGoogleLogin, startLoginEmailPassword } from '../../actions/auth';
 import { useForm } from '../../hooks/useForm';
 
 export const LoginScreen = () => {
+    const {loading} = useSelector(state => state.ui)
     const dispatch = useDispatch();
 
     const [formValues, handleInputChange] = useForm({
@@ -30,7 +31,7 @@ export const LoginScreen = () => {
                 <input type='text' placeholder='E-mail' name='email' className='auth__input' autoComplete='off' value={email} onChange={handleInputChange} />
                 <input type='password' placeholder='Password' name='password' className='auth__input' value={password} onChange={handleInputChange} />
 
-                <button type='submit' className='btn btn-primary btn-block'>
+                <button type='submit' className='btn btn-primary btn-block' disabled={loading}>
                     Login
                 </button>
 
