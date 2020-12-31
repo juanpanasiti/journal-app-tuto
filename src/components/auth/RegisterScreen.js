c154445c65a72c66ce3d695a8c8913c24abd269c
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-12-31 14:06:20 -0300

    Reducer con mensajes de error de registro

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,22 +1,66 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import validator from 'validator'
+import { removeError, setError } from '../../actions/ui';
+
+import { useForm } from '../../hooks/useForm';
 
 export const RegisterScreen = () => {
+    const dispatch = useDispatch()
+
+    const [formValues, handleInputChange] = useForm({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+    });
+
+    const { name, email, password, password2 } = formValues;
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+
+        if (isValidForm()) {
+            console.log('Formulario correcto!')
+        }
+        
+    };
+
+    const isValidForm = () => {
+        if(validator.isEmpty(name)){
+            dispatch(setError('Name is required'))
+            return false
+        } else if (!validator.isEmail(email)){
+            dispatch(setError('Email is not valid'))
+            return false
+        } else if (password !== password2 || password.length < 6){
+            dispatch(setError('Password is not valid'))
+            return false
+        }
+
+        dispatch(removeError())
+        return true
+    }
     return (
         <>
             <h3 className='auth__title'>Register</h3>
 
-            <form>
-                <input type='text' placeholder='Name' name='name' className='auth__input' autoComplete='off' />
-                <input type='text' placeholder='E-mail' name='email' className='auth__input' autoComplete='off' />
-                <input type='password' placeholder='Password' name='password' className='auth__input' />
-                <input type='password' placeholder='Confirm pass' name='password2' className='auth__input' />
+            <form onSubmit={handleRegister}>
+
+                <div className='auth__alert-error'>
+                    Hola Mundo
+                </div>
+
+                <input type='text' placeholder='Name' name='name' value={name} className='auth__input' autoComplete='off' onChange={handleInputChange} />
+                <input type='text' placeholder='E-mail' name='email' value={email} className='auth__input' autoComplete='off' onChange={handleInputChange} />
+                <input type='password' placeholder='Password' name='password' value={password} className='auth__input' onChange={handleInputChange} />
+                <input type='password' placeholder='Confirm pass' name='password2' value={password2} className='auth__input' onChange={handleInputChange} />
 
                 <button type='submit' className='btn btn-primary btn-block mb-5'>
                     Register
                 </button>
 
-                
                 <Link className='link' to='/auth/login'>
                     Aready registered?
                 </Link>
